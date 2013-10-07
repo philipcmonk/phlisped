5428f256f9ee7866e81eb14c1a76c1f4a9714925
Philip Monk <pcmonk@asu.edu>
2013-10-07 14:42:10 -0700

    Fixed all-text search.

--- commands/search.rkt
+++ commands/search.rkt
@@ -15,48 +15,45 @@
 (define Search-text "")
 (define Search-tree '())
 
-;(define (search event)
-; (set! Search-text "")
-; (enter-search-mode))
-;
-;(define (handle-search event)
-; (with
-;  ((let ((c (send event get-key-code)))
-;    (cond
-;     ((and (eq? c #\n) (send event get-control-down))
-;      (scroll-search-results)
-;      (show-search-tree get-rep)
-;      (send Thecanvas on-paint))
-;     ((and (char? c) (not (char-whitespace? c)) (not (char-iso-control? c)) (not (member c '(#\( #\) #\[ #\] #\{ #\} #\" #\, #\' #\` #\; #\# #\| #\\))))
-;      (set! Search-text (string-append Search-text (string (send event get-key-code))))
-;      (show-results))
-;     ((eq? c #\backspace)
-;      (set! Search-text (substring Search-text 0 (- (string-length Search-text) 1)))
-;      (show-results))
-;     ((eq? c #\return)
-;      (exit-search-mode))
-;     ((eq? c 'escape)
-;      (remove-search-tree)
-;      (send Thecanvas on-paint)
-;      (exit-search-mode))
-;     (#t '()))))
-;
-;  (show-results ()
-;   (set-search-results (map triple->list (search-text Search-text)))
-;   (show-search-tree get-rep)
-;   (set-info Search-text)
-;   (send Thecanvas on-paint))))
+(define (search event)
+ (set! Search-text "")
+ (enter-search-mode))
 
-(define data '())
+(define (handle-search event)
+ (with
+  ((let ((c (send event get-key-code)))
+    (cond
+     ((and (eq? c #\n) (send event get-control-down))
+      (scroll-search-results)
+      (show-search-tree get-rep)
+      (send Thecanvas on-paint))
+     ((and (char? c) (not (char-whitespace? c)) (not (char-iso-control? c)) (not (member c '(#\( #\) #\[ #\] #\{ #\} #\" #\, #\' #\` #\; #\# #\| #\\))))
+      (set! Search-text (string-append Search-text (string (send event get-key-code))))
+      (show-results))
+     ((eq? c #\backspace)
+      (set! Search-text (substring Search-text 0 (- (string-length Search-text) 1)))
+      (show-results))
+     ((eq? c #\return)
+      (exit-search-mode))
+     ((eq? c 'escape)
+      (remove-search-tree)
+      (send Thecanvas on-paint)
+      (exit-search-mode))
+     (#t '()))))
 
-;(define (search-text text)
-; (let* ((regex (regexp text))
-;        (texts (map (lambda (id) (graph-neighborhood-edge-forward G id "is written")) (graph-ids G)))
-;        (texts2 (filter (lambda (text) (not (null? text))) texts))
-;        (texts3 (map car texts2)))
-;  (filter (lambda (t) (regexp-match? regex (format "~a" (triple-end t)))) texts3)))
+  (show-results ()
+   (set-search-results (search-text Search-text))
+   (show-search-tree get-rep)
+   (set-info Search-text)
+   (send Thecanvas on-paint))))
 
-;(define data
-; (list
-;  #\/ search
-;  'search handle-search))
+(define (search-text text)
+ (let* ((regex (regexp text))
+        (texts (hash-map (hash-remove G 'next-id)  (lambda (id gn) (list id '() (gnode-name gn)))))
+        (texts2 (filter (lambda (text) (not (null? text))) texts)))
+  (filter (lambda (t) (regexp-match? regex (format "~a" (caddr t)))) texts2)))
+
+(define data
+ (list
+  #\/ search
+  'search handle-search))
